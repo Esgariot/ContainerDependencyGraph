3f8c2bdb382b0696a0a9497ad67078f9bdf92c9e
Robert Marciniak <robert.marciniak@ihsmarkit.com>
2019-12-12 13:15:24 +0100

    feat: Add Utility project

---
+++ src/Utility/Seq.fs
@@ -0,0 +1,12 @@
+﻿namespace Utility
+open System.Collections.Generic
+open FSharpPlus
+
+module Seq =
+    let intersection (left: 'a seq) (right: 'a seq) =
+        let cache = HashSet<'a>(right, HashIdentity.Structural)
+        left |> filter cache.Contains
+
+    let difference  (left: 'a seq) (right: 'a seq)  =
+        let cache = HashSet< 'a >(right, HashIdentity.Structural)
+        left |> filter (cache.Contains >> not)
---
+++ src/Utility/TypeAnalysis.fs
@@ -0,0 +1,27 @@
+﻿namespace Utility
+
+module TypeAnalysis =
+    open System
+    open System.Collections
+    open System.Linq
+    open FSharpPlus
+    open System.Reflection
+
+    let isEnumerableType (givenType: Type) =
+        givenType.IsAssignableFrom(typeof<IEnumerable>)
+
+    let rec isAssignableToGenericType (genericType: Type) (givenType: Type) =
+        let isMappableTo (generic: Type) (given: Type) =
+            given.IsGenericType && given.GetGenericTypeDefinition() = generic
+        let isAssignable (generic: Type) (given: Type) =
+            given.GetInterfaces().Append(given) |>> (isMappableTo generic) |> exists id
+        let rec isThisOrBaseAssignable (generic: Type) (given: Type) =
+            match generic, given with
+            | null, null -> true
+            | _, null
+            | null, _ -> false
+            | _ -> (isAssignable generic given) || (isThisOrBaseAssignable generic given.BaseType)
+        isThisOrBaseAssignable genericType givenType
+
+    let genericParameterTypes (givenType: Type) =
+        givenType.GetTypeInfo().GenericTypeArguments |> seq
